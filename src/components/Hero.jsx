f55503aca3ccb36e5d0372b87ab8e56bd879ad1e
Girikrishna Gireeshkumar <girikrishnagireeshkumar@Girikrishnas-Laptop.local>
2024-07-15 08:10:28 +0300

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,9 @@ const Hero = () => {
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
         
        <h1 className="logo__fr">BRIEFLY.</h1>
-        
+       
       </nav>
+     
 
       <h1 className='head_text'>
         Summarize Articles with <br className='max-md:hidden' />
